70e20dbf16c4fbe1ded8fd228abcd0dec93980a6
GALAXYBOOK-DONG\ss099 <ss099ss000@gmail.com>
2025-03-19 14:51:21 +0900

    generate

---
+++ moneylog_spring/src/main/java/bitc/fullstack503/moneylog_spring/MoneylogSpringApplication.java
@@ -0,0 +1,11 @@
+package bitc.fullstack503.moneylog_spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MoneylogSpringApplication
+{
+  public static void main (String[] args)
+  {
+    SpringApplication.run (MoneylogSpringApplication.class, args);
+  }
+}
---
+++ moneylog_spring/src/test/java/bitc/fullstack503/moneylog_spring/MoneylogSpringApplicationTests.java
@@ -0,0 +1,11 @@
+package bitc.fullstack503.moneylog_spring;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class MoneylogSpringApplicationTests
+{
+  @Test
+  void contextLoads ()
+  {
+  }
+}
