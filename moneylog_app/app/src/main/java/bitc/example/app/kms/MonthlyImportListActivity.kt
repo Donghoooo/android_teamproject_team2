0a8d7ddb458d80fbf2a6604c2d628e6584e8a6eb
CodeMinSeo <rnrmf6076@gmail.com>
2025-03-20 18:22:12 +0900

    월별 리스트

--- moneylog_app/app/src/main/java/bitc/example/app/kms/MonthlyImportListActivity.kt
+++ moneylog_app/app/src/main/java/bitc/example/app/kms/MonthlyImportListActivity.kt
@@ -1,21 +1,18 @@
-package bitc.example.app
+package bitc.example.app.kms
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import bitc.example.app.databinding.ActivityMonthlyListBinding
+import bitc.example.app.R
 
-class MonthlyImportList : AppCompatActivity() {
+class MonthlyImportListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        val binding = ActivityMonthlyListBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        setContentView(R.layout.activity_monthly_import_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
