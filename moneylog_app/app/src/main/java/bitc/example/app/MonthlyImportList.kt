4276d1168c1b09f546ba91a6f78fa19b34fe2492
CodeMinSeo <rnrmf6076@gmail.com>
2025-03-20 17:01:47 +0900

    리스트 카테고리 버튼

--- moneylog_app/app/src/main/java/bitc/example/app/MonthlyImportList.kt
+++ moneylog_app/app/src/main/java/bitc/example/app/MonthlyImportList.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import bitc.example.app.databinding.ActivityMonthlyListBinding
 
-class MonthlyList : AppCompatActivity() {
+class MonthlyImportList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
