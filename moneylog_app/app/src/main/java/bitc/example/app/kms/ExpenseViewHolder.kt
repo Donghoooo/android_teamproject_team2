ea40c1fb108f6dfb90b366a0709832d6c9bfc5de
CodeMinSeo <rnrmf6076@gmail.com>
2025-03-24 16:54:34 +0900

    병합

---
+++ moneylog_app/app/src/main/java/bitc/example/app/kms/ExpenseViewHolder.kt
@@ -0,0 +1,6 @@
+package bitc.example.app.kms
+
+import androidx.recyclerview.widget.RecyclerView
+import bitc.example.app.databinding.ExpenseItemRecyclerViewBinding
+
+class ExpenseViewHolder(val binding: ExpenseItemRecyclerViewBinding): RecyclerView.ViewHolder(binding.root)
